fc41d5fb24fed7bc991ac4d73d9adc01c979cc6f
christopher mcintyre <chrsintyre@gmail.com>
2016-02-02 13:46:26 +0000

    Populate chapters list view with relevant data

--- app/src/main/java/com/davidmoodie/compmathsreference/ChaptersActivity.java
+++ app/src/main/java/com/davidmoodie/compmathsreference/ChaptersActivity.java
@@ -1,11 +1,21 @@
 package com.davidmoodie.compmathsreference;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ChaptersActivity extends AppCompatActivity {
+    private ArrayList<IDString> chapters;
+    private ArrayAdapter<IDString> chaptersAdapter;
+    private ListView lvChapters;
+    private int topicID;
+
+    private DataBaseHelper myDbHelper = new DataBaseHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,22 +23,27 @@ public class ChaptersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chapters);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        myDbHelper.openDataBase();
 
-        TextView textView = (TextView) findViewById(R.id.textView);
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            int topicID = extras.getInt("topicID");
+            topicID = extras.getInt("topicID");
             //textView.setText(topicID + " " + textView.getText());
         }
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        populateChaptersListView();
     }
 
+    public void populateChaptersListView() {
+        lvChapters = (ListView) findViewById(R.id.lvChapters);
+        chapters = new ArrayList<IDString>();
+        chaptersAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, chapters);
+        lvChapters.setAdapter(chaptersAdapter);
+        Cursor cursor = myDbHelper.selectQuery("SELECT _id, name FROM chapter WHERE topic_id=" + topicID + " ;");
+        if (cursor.moveToFirst()) {
+            do {
+                chapters.add(new IDString(cursor.getInt(0), cursor.getString(1)));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+    }
 }
