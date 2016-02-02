ae2d233fd09c274c3915f22e65019b7465fbb65f
christopher mcintyre <chrsintyre@gmail.com>
2016-02-02 14:23:56 +0000

    Load relevant markdown on creation of ChapterActivity

--- app/src/main/java/com/davidmoodie/compmathsreference/ChapterContent.java
+++ app/src/main/java/com/davidmoodie/compmathsreference/ChapterContent.java
@@ -1,5 +1,6 @@
 package com.davidmoodie.compmathsreference;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,9 +9,9 @@ import android.widget.Toast;
 import us.feras.mdv.MarkdownView;
 
 public class ChapterContent extends AppCompatActivity {
-
-    MarkdownView markdownView;
-    int chapterID;
+    private DataBaseHelper myDbHelper = new DataBaseHelper(this);
+    private MarkdownView markdownView;
+    private int chapterID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +26,17 @@ public class ChapterContent extends AppCompatActivity {
             Toast.makeText(ChapterContent.this, "Chapter ID = " + chapterID, Toast.LENGTH_SHORT).show();
         }
 
+        myDbHelper.openDataBase();
         markdownView = (MarkdownView) findViewById(R.id.markdownView);
-        markdownView.loadMarkdown("## Hello Markdown");
+        markdownView.loadMarkdown(this.getChapterMarkdown(chapterID));
     }
 
+    public String getChapterMarkdown(int id) {
+        Cursor cursor = myDbHelper.selectQuery("SELECT markdown FROM chapter WHERE _id=" + id + " ;");
+        if (cursor.moveToFirst()) {
+            return cursor.getString(0);
+        }else {
+            return "### These aren't the markdown your looking for :(";
+        }
+    }
 }
