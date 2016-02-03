d5b6b6eac8eaa49e97bb6c996c98beeddbb391db
christopher mcintyre <chrsintyre@gmail.com>
2016-02-03 16:36:35 +0000

    Fleshed out markdown generation script **UNTESTED**

--- app/src/parse_markdown_to_db.py
+++ app/src/parse_markdown_to_db.py
@@ -1,37 +1,49 @@
+# Python3 script to parse markdown and save to database
+
+## WARNING THIS IS ALL UNTESTED! ##
+## Commented out for safety when ##
+## running gradle builds.        ##
+
+# import os
+# import sqlite3
+# import markdown2 
+
+# CWD = os.getcwd()
+
+# MD_DIR = 'markdown'
+# MD_PATH = os.path.join(CWD, MD_DIR)
+
+# DB_NAME = 'compmaths.db'
+# DB_PATH = os.path.join(CWD, 'main', 'assets', DB_NAME)
+# TABLE = 'chapter'
+# COLUMN = 'markdown'
 
 
-DB_PATH = # Full path to database
-MD_PATH = # Path to markdown directory
-
-DB = # open database at DB_PATH
-TABLE = 'chapter'
-COLUMN = 'markdown'
-	
-def parse_markdown(file):
-	# open_file
-	# parse_markdown
-	# close_file
-	# return parsed_markdown
-	
-	
-def get_chapter_id(file):
-	# return file[0:2]
+# def parse_markdown(md):
+	# """Parse markdown from given file"""
+	# with open(md) as m:
+		# return markdown2.markdown(m.read())
 
 	
-def write_text_to_db(database, table, column, topic_id, chapter_num, text):
-	'UPDATE {table} SET {column} = "{text}", chapter_num = {chapter_num} WHERE topic_id = {topic_id};'.format(
-		table, column, text, chapter_num, topic_id)
+# def update_chapter_markdown(cursor, topic_id, chapter_num, text):
+	# """Execute SQL update query on chapter table's markdown field"""
+	# cursor.execute('UPDATE {table} SET {column} = "{text}", chapter_num = {chapter_num} WHERE topic_id = {topic_id};'.format(
+		# table=TABLE, column=COLUMN, topic_id, chapter_num, text))
 	
 	
-def update_db(path):
-	topic_counter = 1
-	for topic in MD_PATH:
-		for idx, f in enumerate(os.path.join(MD_PATH, topic)):
-			md = parse_markdown(f)
-			write_text_to_db(DB, TABLE, COLUMN, topic_counter, idx+1, md)
-		topic_counter += 1
+# def update_db(path, cursor):
+	# """Cycle through markdown folders and update database using cursor"""
+	# topic_counter = 1
+		# for topic in os.listdir(MD_PATH):
+			# ch_dir = os.path.join(MD_PATH, topic)
+			# for i, md in enumerate(ch_dir):
+				# m = parse_markdown(os.path.join(ch_dir, md))
+				# update_chapter_markdown(cursor, topic_counter, i+1, md)
+		# topic_counter += 1
 
-if __name__ == '__main__':
-	update_db()
-	
-(0, '01_intro'), (1, '02_whaterver')
+# if __name__ == '__main__':
+	# db = sqlite3.connect(DB_PATH)
+	# c = db.cursor()
+	# update_db(DB_PATH, c)
+	# db.commit()
+	# db.close()
