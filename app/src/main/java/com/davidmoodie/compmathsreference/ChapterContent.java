ff696fbd71f5e20b12d946b48676cdaaebac359b
David Moodie <davidmoodie12@gmail.com>
2016-02-04 21:14:48 +0000

    Added a quiz button to the toolbar in the chapter content activity.

--- app/src/main/java/com/davidmoodie/compmathsreference/ChapterContent.java
+++ app/src/main/java/com/davidmoodie/compmathsreference/ChapterContent.java
@@ -4,6 +4,9 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.widget.Toast;
 
@@ -37,5 +40,28 @@ public class ChapterContent extends AppCompatActivity {
         }else {
             return "### These aren't the markdowns you're looking for\n There was an error retrieving requested chapter content";
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_chapter, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_quiz) {
+            Log.d("DEBUG", "onOptionsItemSelected: QUIZ CLICKED");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
